d57562fcbf38d284075f9f7df4cbd0a2f6893009
JoaoVitor-Penitente <joaov.penitente@gmail.com>
2021-09-03 21:24:42 -0300

    Segundo Commit

--- Filmes/routes/web.php
+++ Filmes/routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\MovieController;
+use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('movie', MovieController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
